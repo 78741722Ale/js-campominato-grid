7f089539d606d2f09fb98c7fdf45d8b8379f2181
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-04 18:58:45 +0200

    Prime righe di codice di JS (fermo per difficoltà)

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// Consegna :
+
+/* 
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, 
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+*/
+
+// Generare valore del select del form
+const userDifficult = document.getElementById("difficulties").value;
+// Genera valori delle option
+const easyChoiche = document.getElementById("easy").value;
+const mediumChoiche = document.getElementById("medium").value;
+const difficultChoiche = document.getElementById("difficult").value;
+// Bottone Play
+const playButton = document.getElementById("play_btn");
+// Tutti console log delle scelte e option
+console.log(userDifficult, easyChoiche, mediumChoiche, difficultChoiche, playButton);
